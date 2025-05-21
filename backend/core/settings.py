7a09ed9a0226edaafc7511ee61b5eb8899d5515a
rdzantoine.pro@gmail.com <rdzantoine.pro@gmail.com>
2025-05-21 13:47:09 +0200

    feat: add a docker-compose.yml file to use postgres in development, add psycopg2 and pillow packages in requirements, working migrations and runserver with postgres

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,8 +13,16 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+# For django-environ
+import environ
+
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wcsv7*z5)&7sms8!5u3#xp71dd6yxahi4-cj)s%g7e=xr%f*uw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -82,12 +90,36 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            # "ENGINE": "django.db.backends.postgresql",
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": 'postgres',
+            "USER": 'postgres',
+            "PASSWORD": 'postgres',
+            "HOST": 'localhost',
+            "PORT": '5432',
+        }
     }
-}
+else :
+    DATABASES = {
+        'default': {
+            # "ENGINE": "django.db.backends.postgresql",
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": env('DATABASE_NAME'),
+            "USER": env('DATABASE_USER'),
+            "PASSWORD": env('DATABASE_PASSWORD'),
+            "HOST": env('DATABASE_HOST'),  # set in docker-compose.yml
+            "PORT": env('DATABASE_PORT'),  # default postgres port
+        }
+    }
 
 
 # Password validation
