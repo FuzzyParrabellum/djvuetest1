dbfb9803888a8e3b60ad9600e14c089797d24d64
rdzantoine.pro@gmail.com <rdzantoine.pro@gmail.com>
2025-05-28 13:09:32 +0200

    style: remove a few empty lines

--- backend/coreapps/blog/schema.py
+++ backend/coreapps/blog/schema.py
@@ -25,72 +25,51 @@ class TagType(DjangoObjectType):
 class Query(graphene.ObjectType):
 
     all_posts = graphene.List(PostType)
-
-    author_by_username = graphene.Field(AuthorType, username=graphene.String())
-
+    author_by_username = graphene.Field(AuthorType, name=graphene.String())
     post_by_slug = graphene.Field(PostType, slug=graphene.String())
-
-    posts_by_author = graphene.List(PostType, username=graphene.String())
-
+    posts_by_author = graphene.List(PostType, name=graphene.String())
     posts_by_tag = graphene.List(PostType, tag=graphene.String())
 
 
     def resolve_all_posts(root, info):
 
         return (
-
             models.Post.objects.prefetch_related("tags")
-
             .select_related("author")
-
             .all()
-
         )
 
 
     def resolve_author_by_username(root, info, name):
 
         return models.Profile.objects.select_related("user").get(
-
             user__name=name
-
         )
 
 
     def resolve_post_by_slug(root, info, slug):
 
         return (
-
             models.Post.objects.prefetch_related("tags")
-
             .select_related("author")
-
             .get(slug=slug)
-
         )
 
 
     def resolve_posts_by_author(root, info, name):
 
         return (
-
             models.Post.objects.prefetch_related("tags")
-
             .select_related("author")
-
             .filter(author__user__name=name)
-
         )
 
 
     def resolve_posts_by_tag(root, info, tag):
 
         return (
-
             models.Post.objects.prefetch_related("tags")
-
             .select_related("author")
-
             .filter(tags__name__iexact=tag)
 
         )
